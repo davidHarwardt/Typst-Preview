56d36ba1ce9a7ed49d33e03494b25ecf753e0ab3
davidHarwardt <david.harwardt@icloud.com>
2026-03-09 20:43:43 +0100

    Initial Commit

---
+++ Typst Preview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Typst Preview
+//
+//  Created by David Harwardt on 09.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Typst Preview/Typst_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Typst_PreviewApp.swift
+//  Typst Preview
+//
+//  Created by David Harwardt on 09.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Typst_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
